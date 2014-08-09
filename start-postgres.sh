0920fd6147e954e91288ca4d6fe1a535af8bc94d
Craig R Webster <craig@barkingiguana.com>
2014-08-09 09:06:47 +0100

    Initial commit

---
+++ start-postgres.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -d /var/run/postgresql ]; then
+    chmod 2775 /var/run/postgresql
+else
+    install -d -m 2775 -o postgres -g postgres /var/run/postgresql
+fi
+
+exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
